b111b464eee11f10d0d7070a8a6fd43108cba9a0
Joshan <user@example.com>
2026-04-09 23:48:19 +0300

    Fix IPA load issues and surgical cleanup of 80+ junk files

--- add_metadata_to_payments.sql
+++
@@ -1,2 +0,0 @@
--- Add metadata column to payments table
-ALTER TABLE "payments" ADD COLUMN IF NOT EXISTS "metadata" JSONB DEFAULT '{}'::jsonb;
--- add_wallet_column.sql
+++
@@ -1,2 +0,0 @@
--- Add wallet_balance column to users table
-ALTER TABLE users ADD COLUMN IF NOT EXISTS "wallet_balance" DOUBLE PRECISION DEFAULT 0;
